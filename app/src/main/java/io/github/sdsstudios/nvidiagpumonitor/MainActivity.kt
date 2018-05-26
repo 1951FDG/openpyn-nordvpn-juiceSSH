70734c4cb64b56b9d69dd0656df6bd823438a945
1951FDG <1951FDG@users.noreply.github.com>
2018-05-26 06:14:37 +0200

    Add Google Maps

--- app/src/main/java/io/github/sdsstudios/nvidiagpumonitor/MainActivity.kt
+++ app/src/main/java/io/github/sdsstudios/nvidiagpumonitor/MainActivity.kt
@@ -1,36 +1,50 @@
 package io.github.sdsstudios.nvidiagpumonitor
 
-import android.arch.lifecycle.Observer
+//import kotlinx.android.synthetic.main.content_main.*
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.content.res.Resources
 import android.database.Cursor
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.AppCompatTextView
+//import android.support.v7.widget.AppCompatTextView
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.ImageView
+import android.widget.RelativeLayout
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.*
 import com.sonelli.juicessh.pluginlibrary.listeners.OnClientStartedListener
 import com.sonelli.juicessh.pluginlibrary.listeners.OnSessionFinishedListener
 import com.sonelli.juicessh.pluginlibrary.listeners.OnSessionStartedListener
 import io.github.sdsstudios.nvidiagpumonitor.ConnectionManager.Companion.JUICESSH_REQUEST_CODE
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.content_main.*
+import kotlinx.android.synthetic.main.activity_maps.*
 import org.jetbrains.anko.toast
+
 
 class MainActivity : AppCompatActivity(),
         OnSessionStartedListener,
         OnSessionFinishedListener,
         OnClientStartedListener,
-        ConnectionListLoaderFinishedCallback {
-
+        ConnectionListLoaderFinishedCallback,
+        OnMapReadyCallback,
+        GoogleMap.OnMarkerClickListener {
     companion object {
         private const val READ_CONNECTIONS = "com.sonelli.juicessh.api.v1.permission.READ_CONNECTIONS"
         private const val OPEN_SESSIONS = "com.sonelli.juicessh.api.v1.permission.OPEN_SESSIONS"
         private const val PERMISSION_REQUEST_CODE = 23
         private const val JUICE_SSH_PACKAGE_NAME = "com.sonelli.juicessh"
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 0
     }
 
     private var mReadConnectionsPerm = false
@@ -49,10 +63,18 @@ class MainActivity : AppCompatActivity(),
     private val mPermissionsGranted
         get() = mReadConnectionsPerm && mOpenSessionsPerm
 
+    private lateinit var mMap: GoogleMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
         if (isJuiceSSHInstalled()) {
 
@@ -60,37 +82,37 @@ class MainActivity : AppCompatActivity(),
 
             requestPermissions()
 
-            mConnectionManager.powerUsage.observe(this, Observer {
-                textViewPower.setData(it, "W")
-            })
-
-            mConnectionManager.temperature.observe(this, Observer {
-                textViewTemp.setData(it, "C")
-            })
-
-            mConnectionManager.fanSpeed.observe(this, Observer {
-                textViewFanSpeed.setData(it, "%")
-            })
-
-            mConnectionManager.freeMemory.observe(this, Observer {
-                textViewFreeMemory.setData(it, "MB")
-            })
-
-            mConnectionManager.usedMemory.observe(this, Observer {
-                textViewUsedMemory.setData(it, "MB")
-            })
-
-            mConnectionManager.graphicsClock.observe(this, Observer {
-                textViewClockGraphics.setData(it, "MHz")
-            })
-
-            mConnectionManager.videoClock.observe(this, Observer {
-                textViewClockVideo.setData(it, "MHz")
-            })
-
-            mConnectionManager.memoryClock.observe(this, Observer {
-                textViewClockMemory.setData(it, "MHz")
-            })
+//            mConnectionManager.powerUsage.observe(this, Observer {
+//                textViewPower.setData(it, "W")
+//            })
+//
+//            mConnectionManager.temperature.observe(this, Observer {
+//                textViewTemp.setData(it, "C")
+//            })
+//
+//            mConnectionManager.fanSpeed.observe(this, Observer {
+//                textViewFanSpeed.setData(it, "%")
+//            })
+//
+//            mConnectionManager.freeMemory.observe(this, Observer {
+//                textViewFreeMemory.setData(it, "MB")
+//            })
+//
+//            mConnectionManager.usedMemory.observe(this, Observer {
+//                textViewUsedMemory.setData(it, "MB")
+//            })
+//
+//            mConnectionManager.graphicsClock.observe(this, Observer {
+//                textViewClockGraphics.setData(it, "MHz")
+//            })
+//
+//            mConnectionManager.videoClock.observe(this, Observer {
+//                textViewClockVideo.setData(it, "MHz")
+//            })
+//
+//            mConnectionManager.memoryClock.observe(this, Observer {
+//                textViewClockMemory.setData(it, "MHz")
+//            })
 
             if (mPermissionsGranted) {
                 onPermissionsGranted()
@@ -176,7 +198,7 @@ class MainActivity : AppCompatActivity(),
 
     override fun onSessionStarted(sessionId: Int, sessionKey: String?) {
         buttonConnect.applyDisconnectStyle()
-        cardViewLayout.visibility = View.VISIBLE
+        //cardViewLayout.visibility = View.VISIBLE
         spinnerConnectionList.isEnabled = false
     }
 
@@ -186,7 +208,7 @@ class MainActivity : AppCompatActivity(),
 
     override fun onSessionFinished() {
         buttonConnect.applyConnectStyle()
-        cardViewLayout.visibility = View.GONE
+        //cardViewLayout.visibility = View.GONE
         spinnerConnectionList.isEnabled = true
     }
 
@@ -216,14 +238,14 @@ class MainActivity : AppCompatActivity(),
         ))
     }
 
-    private fun AppCompatTextView.setData(value: Int?, suffix: String) {
-        if (value == null) {
-            setText(R.string.no_data)
-        } else {
-            val data = "$value $suffix"
-            text = data
-        }
-    }
+//    private fun AppCompatTextView.setData(value: Int?, suffix: String) {
+//        if (value == null) {
+//            setText(R.string.no_data)
+//        } else {
+//            val data = "$value $suffix"
+//            text = data
+//        }
+//    }
 
     private fun requestPermissions() {
         mReadConnectionsPerm = hasPermission(READ_CONNECTIONS)
@@ -248,5 +270,67 @@ class MainActivity : AppCompatActivity(),
         } catch (e: PackageManager.NameNotFoundException) {
             return false
         }
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        googleMap.setOnMarkerClickListener(this)
+        //googleMap.isMyLocationEnabled = true
+
+        map_layout.visibility = View.VISIBLE
+
+        try {
+            val success = googleMap.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                            this, R.raw.style_json))
+
+            if (!success) {
+                Log.e(TAG, "Style parsing failed.")
+            }
+        } catch (e: Resources.NotFoundException) {
+            Log.e(TAG, "Can't find style. Error: ", e)
+        }
+
+        val sydney = LatLng(-34.0, 151.0)
+        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        //googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 12.0f))
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        val watermark = mapFragment.view?.findViewWithTag<ImageView>("GoogleWatermark")
+        val params = watermark?.layoutParams as RelativeLayout.LayoutParams
+        params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, 0)
+        params.addRule(RelativeLayout.ALIGN_PARENT_LEFT, 0)
+        //params.addRule(RelativeLayout.ALIGN_PARENT_START, RelativeLayout.TRUE)
+        params.addRule(RelativeLayout.ALIGN_PARENT_TOP, RelativeLayout.TRUE)
+        //params.addRule(RelativeLayout.ALIGN_PARENT_END, 0)
+
+        googleMap.setPadding(0,0,0,buttonConnect.height + buttonConnect.paddingBottom)
+
+        if (ActivityCompat.checkSelfPermission(this,
+                        android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
+        }
+
+        fusedLocationClient.lastLocation.addOnSuccessListener(this) { location ->
+            if (location != null) {
+                googleMap.addMarker(MarkerOptions().position(LatLng(location.latitude, location.longitude)).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)))
+                //googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 12.0f))
+            }
+        }
+    }
+
+    override fun onMarkerClick(p0: Marker?): Boolean {
+        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return false
     }
 }
