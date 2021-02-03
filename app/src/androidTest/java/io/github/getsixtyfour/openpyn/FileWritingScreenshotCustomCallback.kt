f2fd53be811207d0eb92b1b755db2ce8ac475f6e
1951FDG <1951FDG@users.noreply.github.com>
2021-02-03 20:38:56 +0100

    Convert FileWritingScreenshotCustomCallback

---
+++ app/src/androidTest/java/io/github/getsixtyfour/openpyn/FileWritingScreenshotCustomCallback.kt
@@ -0,0 +1,13 @@
+package io.github.getsixtyfour.openpyn
+
+import android.content.Context
+import org.jetbrains.annotations.NonNls
+import tools.fastlane.screengrab.FileWritingScreenshotCallback
+import java.io.File
+
+class FileWritingScreenshotCustomCallback(appContext: Context?) : FileWritingScreenshotCallback(appContext) {
+    override fun getScreenshotFile(screenshotDirectory: File, screenshotName: String): File {
+        @NonNls val screenshotFileName = "$screenshotName.png"
+        return File(screenshotDirectory, screenshotFileName)
+    }
+}
