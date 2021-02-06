713d276d615165103d6d431abcb4faa857fd8001
1951FDG <1951FDG@users.noreply.github.com>
2021-02-06 22:43:10 +0100

    Set all AppConfig values to false, these are app flags to be turned on manually, but should be off by default

--- app/src/debug/java/io/github/getsixtyfour/openpyn/AppConfig.java
+++ app/src/debug/java/io/github/getsixtyfour/openpyn/AppConfig.java
@@ -3,15 +3,15 @@ package io.github.getsixtyfour.openpyn;
 @SuppressWarnings("UtilityClass")
 public final class AppConfig {
 
-    public static final boolean EMULATOR = Boolean.TRUE;
+    public static final boolean EMULATOR = Boolean.FALSE;
 
-    public static final boolean ONLINE = Boolean.TRUE;
+    public static final boolean ONLINE = Boolean.FALSE;
 
-    public static final boolean STETHO = Boolean.TRUE;
+    public static final boolean STETHO = Boolean.FALSE;
 
-    public static final boolean STRICT_MODE = Boolean.TRUE;
+    public static final boolean STRICT_MODE = Boolean.FALSE;
 
-    public static final boolean UI_BLOCK_DETECTION = Boolean.TRUE;
+    public static final boolean UI_BLOCK_DETECTION = Boolean.FALSE;
 
     private AppConfig() {
     }
