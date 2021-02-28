cf9afdfad2a653e8ba19a1cb189c40f00b2717cc
1951FDG <1951FDG@users.noreply.github.com>
2021-02-28 14:25:14 +0100

    Remove Toasty and add own implementation of material toasts

--- app/src/main/kotlin/io/github/getsixtyfour/openpyn/utils/Toaster.kt
+++ app/src/main/kotlin/io/github/getsixtyfour/openpyn/utils/Toaster.kt
@@ -1,46 +1,86 @@
 package io.github.getsixtyfour.openpyn.utils
 
+import android.annotation.SuppressLint
 import android.content.Context
-import es.dmoral.toasty.Toasty
+import android.content.res.ColorStateList
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.annotation.CheckResult
+import androidx.annotation.ColorRes
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import io.github.getsixtyfour.openpyn.R
 import io.github.sdsstudios.nvidiagpumonitor.listeners.OnOutputLineListener
 
-class Toaster(context: Context) : OnOutputLineListener {
+/**
+ * @see <a href="https://github.com/xolox/python-coloredlogs/blob/master/coloredlogs/__init__.py">python-coloredlogs</a>
+ * <br>
+ * @see <a href="https://material.io/design/usability/accessibility.html#color-and-contrast">Accessibility - Material Design</a>
+ * <br>
+ * @see <a href="https://github.com/LeaVerou/contrast-ratio">Contrast Ratio</a>
+ */
+open class Toaster(context: Context) : OnOutputLineListener {
 
     private val mContext: Context = context.applicationContext
+    private val mInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
 
     override fun spam(message: CharSequence) {
-        Toasty.info(mContext, message, Toasty.LENGTH_LONG, false).show()
+        makeToast(message, Toast.LENGTH_LONG, R.color.spamColor, R.color.design_dark_default_color_on_background).show()
     }
 
     override fun debug(message: CharSequence) {
-        Toasty.info(mContext, message, Toasty.LENGTH_LONG, false).show()
+        makeToast(message, Toast.LENGTH_LONG, R.color.debugColor, R.color.design_dark_default_color_on_background).show()
     }
 
     override fun verbose(message: CharSequence) {
-        Toasty.info(mContext, message, Toasty.LENGTH_LONG, false).show()
+        makeToast(message, Toast.LENGTH_LONG, R.color.verboseColor, R.color.design_dark_default_color_on_background).show()
     }
 
     override fun info(message: CharSequence) {
-        Toasty.info(mContext, message, Toasty.LENGTH_LONG, false).show()
+        makeToast(message).show()
     }
 
     override fun notice(message: CharSequence) {
-        Toasty.info(mContext, message, Toasty.LENGTH_LONG, false).show()
+        makeToast(message).show()
     }
 
     override fun warning(message: CharSequence) {
-        Toasty.warning(mContext, message, Toasty.LENGTH_LONG, false).show()
+        makeToast(message, Toast.LENGTH_LONG, R.color.warningColor, R.color.design_default_color_on_background).show()
     }
 
     override fun success(message: CharSequence) {
-        Toasty.success(mContext, message, Toasty.LENGTH_LONG, false).show()
+        makeToast(message, Toast.LENGTH_LONG, R.color.successColor, R.color.design_dark_default_color_on_background).show()
     }
 
     override fun error(message: CharSequence) {
-        Toasty.error(mContext, message, Toasty.LENGTH_LONG, false).show()
+        makeToast(message, Toast.LENGTH_LONG, R.color.errorColor, R.color.design_dark_default_color_on_background).show()
     }
 
     override fun critical(message: CharSequence) {
-        Toasty.error(mContext, message, Toasty.LENGTH_LONG, false).show()
+        makeToast(message, Toast.LENGTH_LONG, R.color.criticalColor, R.color.design_dark_default_color_on_background).show()
+    }
+
+    @SuppressLint("InflateParams")
+    @CheckResult
+    protected fun makeToast(
+        message: CharSequence, duration: Int = Toast.LENGTH_LONG, @ColorRes tintColorId: Int? = null, @ColorRes textColorId: Int? = null
+    ): Toast {
+        val view = (mInflater.inflate(R.layout.toast_notification, null) as ViewGroup).apply {
+            tintColorId?.let { ViewCompat.setBackgroundTintList(this, ColorStateList.valueOf(ContextCompat.getColor(mContext, it))) }
+        }
+
+        view.findViewById<TextView>(android.R.id.message).apply {
+            setText(message)
+            textColorId?.let { setTextColor(ContextCompat.getColor(mContext, it)) }
+        }
+
+        return Toast(mContext).apply {
+            setGravity(Gravity.CENTER_HORIZONTAL or Gravity.TOP, 0, yOffset)
+            setDuration(duration)
+            setView(view)
+        }
     }
 }
