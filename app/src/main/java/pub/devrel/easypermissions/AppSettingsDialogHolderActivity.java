ce832364b962cda54281a94b331cf54a72fba142
1951FDG <1951FDG@users.noreply.github.com>
2021-03-10 17:52:59 +0100

    Add missing annotations to AppSettingsDialogHolderActivity

--- app/src/main/java/pub/devrel/easypermissions/AppSettingsDialogHolderActivity.java
+++ app/src/main/java/pub/devrel/easypermissions/AppSettingsDialogHolderActivity.java
@@ -8,6 +8,8 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.annotation.RestrictTo;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,14 +21,15 @@ public class AppSettingsDialogHolderActivity extends AppCompatActivity implement
     private AlertDialog mDialog;
     private int mIntentFlags;
 
-    public static Intent createShowDialogIntent(Context context, AppSettingsDialog dialog) {
+    @NonNull
+    public static Intent createShowDialogIntent(@NonNull Context context, @NonNull AppSettingsDialog dialog) {
         Intent intent = new Intent(context, AppSettingsDialogHolderActivity.class);
         intent.putExtra(AppSettingsDialog.EXTRA_APP_SETTINGS, dialog);
         return intent;
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         AppSettingsDialog appSettingsDialog = AppSettingsDialog.fromIntent(getIntent(), this);
         mIntentFlags = appSettingsDialog.getIntentFlags();
@@ -42,7 +45,7 @@ public class AppSettingsDialogHolderActivity extends AppCompatActivity implement
     }
 
     @Override
-    public void onClick(DialogInterface dialog, int which) {
+    public void onClick(@Nullable DialogInterface dialog, int which) {
         if (which == DialogInterface.BUTTON_POSITIVE) {
             Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
                     .setData(Uri.fromParts("package", getPackageName(), null));
@@ -57,7 +60,7 @@ public class AppSettingsDialogHolderActivity extends AppCompatActivity implement
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         setResult(resultCode, data);
         finish();
